0549c99bf22bdd5e5013e787b3d34742b15e4ed1
bruino <bruino.one@gmail.com>
2021-06-15 21:00:28 -0300

    Agregado y modificado campos en las tablas del modelo BD.

--- controllers/default.py
+++ controllers/default.py
@@ -4,15 +4,38 @@
 # this file is released under public domain and you can use without limitations
 # -------------------------------------------------------------------------
 
+
 def index():
     if db(db.auth_user).count() == 0:
-        db.auth_group.insert(role='admin')
-        user_admin_id = db.auth_user.insert(first_name="admin", email="admin@admin.com",
-                                  password=db.auth_user.password.requires[0]('admin')[0])
-        auth.add_membership('admin', db.auth_user(user_admin_id))
-    return redirect(URL('default', 'provision'))
-    
-    
+        db.auth_group.insert(role="admin")
+        user_admin_id = db.auth_user.insert(
+            first_name="admin",
+            email="admin@admin.com",
+            password=db.auth_user.password.requires[0]("admin")[0],
+        )
+        auth.add_membership("admin", db.auth_user(user_admin_id))
+    if db(db.jerarquia).count() == 0:
+        db.jerarquia.insert(nombre="Agte.")
+        db.jerarquia.insert(nombre="Cabo")
+        db.jerarquia.insert(nombre="Cabo 1er")
+        db.jerarquia.insert(nombre="Sgto")
+        db.jerarquia.insert(nombre="Sgto Ayte.")
+        db.jerarquia.insert(nombre="Sgto 1er")
+        db.jerarquia.insert(nombre="Subof.")
+        db.jerarquia.insert(nombre="Subof. Ppal")
+        db.jerarquia.insert(nombre="Of. Subayte.")
+        db.jerarquia.insert(nombre="Of. Ayte.")
+        db.jerarquia.insert(nombre="Of. Ppal")
+        db.jerarquia.insert(nombre="Subcrio")
+        db.jerarquia.insert(nombre="Crio")
+        db.jerarquia.insert(nombre="Crio Ppal")
+        db.jerarquia.insert(nombre="Crio Insp.")
+        db.jerarquia.insert(nombre="Crio My.")
+        db.jerarquia.insert(nombre="Crio Gral.")
+        db.commit()
+    return redirect(URL("default", "provision"))
+
+
 @auth.requires_login()
 def provision():
     db.Provision.id.readable = db.Provision.id.writable = False
@@ -27,43 +50,69 @@ def provision():
         db.Provision.destino_actual,
     ]
 
-    grid = SQLFORM.smartgrid(db.Provision,
-                             fields=fields,
-                             create=True,
-                             deletable=True,
-                             editable=True,
-                             breadcrumbs_class='breadcrumb',
-                             csv=False,
-                             advanced_search=False,
-                            )
+    grid = SQLFORM.smartgrid(
+        db.Provision,
+        fields=fields,
+        create=True,
+        deletable=True,
+        editable=True,
+        breadcrumbs_class="breadcrumb",
+        csv=False,
+        advanced_search=False,
+    )
     return dict(grid=grid)
 
-@auth.requires_login()
-def admin():
-    grid_user = SQLFORM.smartgrid(db.auth_user, breadcrumbs_class='breadcrumb',)
-    grid_registro = SQLFORM.smartgrid(db.Registro, breadcrumbs_class='breadcrumb',)
-    grid_provision = SQLFORM.smartgrid(db.Provision, breadcrumbs_class='breadcrumb',)
+
+@auth.requires(auth.has_membership("admin"))
+def users():
+    grid_user = SQLFORM.smartgrid(
+        db.auth_user, breadcrumbs_class="breadcrumb", formname="a"
+    )
     return locals()
+
+
+@auth.requires(auth.has_membership("admin"))
+def provisiones():
+    grid_provision = SQLFORM.smartgrid(
+        db.Provision, breadcrumbs_class="breadcrumb", formname="c"
+    )
+    return locals()
+
+
+@auth.requires(auth.has_membership("admin"))
+def registros():
+    grid_registro = SQLFORM.smartgrid(
+        db.Registro, breadcrumbs_class="breadcrumb", formname="b"
+    )
+    return locals()
+
 
 # ---- API (example) -----
 @auth.requires_login()
 def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
+    if not request.env.request_method == "GET":
+        raise HTTP(403)
+    return response.json({"status": "success", "email": auth.user.email})
+
 
 # ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+@auth.requires_membership("admin")  # can only be accessed by members of admin groupd
 def grid():
-    response.view = 'generic.html' # use a generic view
+    response.view = "generic.html"  # use a generic view
     tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    if not tablename in db.tables:
+        raise HTTP(403)
+    grid = SQLFORM.smartgrid(
+        db[tablename], args=[tablename], deletable=False, editable=False
+    )
     return dict(grid=grid)
+
 
 # ---- Embedded wiki (example) ----
 def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
+    auth.wikimenu()  # add the wiki to the menu
+    return auth.wiki()
+
 
 # ---- Action for login/register/etc (required for auth) -----
 def user():
@@ -83,6 +132,7 @@ def user():
     also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
     return dict(form=auth())
+
 
 # ---- action to server uploaded static content (required) ---
 @cache.action()
--- models/db.py
+++ models/db.py
@@ -30,15 +30,17 @@ if not request.env.web2py_runtime_gae:
     # ---------------------------------------------------------------------
     # if NOT running on Google App Engine use SQLite or other DB
     # ---------------------------------------------------------------------
-    db = DAL(configuration.get('db.uri'),
-             pool_size=configuration.get('db.pool_size'),
-             migrate_enabled=configuration.get('db.migrate'),
-             check_reserved=['all'])
+    db = DAL(
+        configuration.get("db.uri"),
+        pool_size=configuration.get("db.pool_size"),
+        migrate_enabled=configuration.get("db.migrate"),
+        check_reserved=["all"],
+    )
 else:
     # ---------------------------------------------------------------------
     # connect to Google BigTable (optional 'google:datastore://namespace')
     # ---------------------------------------------------------------------
-    db = DAL('google:datastore+ndb')
+    db = DAL("google:datastore+ndb")
     # ---------------------------------------------------------------------
     # store sessions and tickets there
     # ---------------------------------------------------------------------
@@ -54,16 +56,17 @@ else:
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
 # -------------------------------------------------------------------------
-response.generic_patterns = [] 
-if request.is_local and not configuration.get('app.production'):
-    response.generic_patterns.append('*')
+response.generic_patterns = []
+if request.is_local and not configuration.get("app.production"):
+    response.generic_patterns.append("*")
 
 # -------------------------------------------------------------------------
 # choose a style for forms
 # -------------------------------------------------------------------------
 from custom_form import custom_formstyle
+
 response.formstyle = custom_formstyle
-response.form_label_separator = ''
+response.form_label_separator = ""
 
 # -------------------------------------------------------------------------
 # (optional) optimize handling of static files
@@ -87,28 +90,30 @@ response.form_label_separator = ''
 # -------------------------------------------------------------------------
 
 # host names must be a list of allowed host names (glob syntax allowed)
-auth = Auth(db, host_names=configuration.get('host.names'))
+auth = Auth(db, host_names=configuration.get("host.names"))
 
 # -------------------------------------------------------------------------
 # create all tables needed by auth, maybe add a list of extra fields
 # -------------------------------------------------------------------------
-auth.settings.extra_fields['auth_user'] = [
-    Field('cargo', 'integer'),
-    Field('chapa', 'integer'),
-    Field('dependencia', 'string'),
+auth.settings.extra_fields["auth_user"] = [
+    Field("cargo", "integer"),
+    Field("chapa", "integer"),
+    Field("dependencia", "string"),
 ]
-auth.settings.actions_disabled = ['retrieve_password']
+auth.settings.actions_disabled = ["retrieve_password"]
 auth.define_tables(username=False, signature=False)
 
 # -------------------------------------------------------------------------
 # configure email
 # -------------------------------------------------------------------------
 mail = auth.settings.mailer
-mail.settings.server = 'logging' if request.is_local else configuration.get('smtp.server')
-mail.settings.sender = configuration.get('smtp.sender')
-mail.settings.login = configuration.get('smtp.login')
-mail.settings.tls = configuration.get('smtp.tls') or False
-mail.settings.ssl = configuration.get('smtp.ssl') or False
+mail.settings.server = (
+    "logging" if request.is_local else configuration.get("smtp.server")
+)
+mail.settings.sender = configuration.get("smtp.sender")
+mail.settings.login = configuration.get("smtp.login")
+mail.settings.tls = configuration.get("smtp.tls") or False
+mail.settings.ssl = configuration.get("smtp.ssl") or False
 
 # -------------------------------------------------------------------------
 # configure auth policy
@@ -117,26 +122,81 @@ auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
-# -------------------------------------------------------------------------  
-# read more at http://dev.w3.org/html5/markup/meta.name.html               
 # -------------------------------------------------------------------------
-response.meta.author = configuration.get('app.author')
-response.meta.description = configuration.get('app.description')
-response.meta.keywords = configuration.get('app.keywords')
-response.meta.generator = configuration.get('app.generator')
-response.show_toolbar = configuration.get('app.toolbar')
+# read more at http://dev.w3.org/html5/markup/meta.name.html
+# -------------------------------------------------------------------------
+response.meta.author = configuration.get("app.author")
+response.meta.description = configuration.get("app.description")
+response.meta.keywords = configuration.get("app.keywords")
+response.meta.generator = configuration.get("app.generator")
+response.show_toolbar = configuration.get("app.toolbar")
 
 # -------------------------------------------------------------------------
-# your http://google.com/analytics id                                      
+# your http://google.com/analytics id
 # -------------------------------------------------------------------------
-response.google_analytics_id = configuration.get('google.analytics_id')
+response.google_analytics_id = configuration.get("google.analytics_id")
 
 # -------------------------------------------------------------------------
 # maybe use the scheduler
 # -------------------------------------------------------------------------
-if configuration.get('scheduler.enabled'):
+if configuration.get("scheduler.enabled"):
     from gluon.scheduler import Scheduler
-    scheduler = Scheduler(db, heartbeat=configuration.get('scheduler.heartbeat'))
+
+    scheduler = Scheduler(db, heartbeat=configuration.get("scheduler.heartbeat"))
+
+# -------------------------------------------------------------------------
+# Include the necessary files by adding this to your model/db.py:
+response.files.append("https://unpkg.com/leaflet@1.5.1/dist/leaflet.css")
+response.files.append("https://unpkg.com/leaflet@1.5.1/dist/leaflet.js")
+response.files.append(
+    "https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css"
+)
+response.files.append(
+    "https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"
+)
+
+# New widget geocoder
+def geocoder_widget(field, value):
+    wrapper = DIV()
+    input_latlng = SQLFORM.widgets.string.widget(field, value, _type="hidden")
+    javascript = SCRIPT(
+        """
+        var map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        var marker
+        
+        function onMapClick(e) {
+            if (marker) map.removeLayer(marker);
+            marker = new L.Marker(e.latlng, { draggable: true });
+            map.addLayer(marker);
+            $("#%s").val(e.latlng.lat + ", " + e.latlng.lng);
+            // marker.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+        }
+        map.on('click', onMapClick);
+        
+        L.Control.geocoder({
+            defaultMarkGeocode: false
+            })
+            .on('markgeocode', function (e) {
+                if (marker) map.removeLayer(marker);
+                marker = new L.Marker(e.geocode.center, { draggable: true });
+                map.addLayer(marker);
+                map.setView(e.geocode.center, 12);
+                $("#%s").val(e.geocode.center.lat + ", " + e.geocode.center.lng);
+            })
+            .addTo(map);
+    """
+        % (input_latlng["_id"], input_latlng["_id"]),
+        _type="text/javascript",
+    )
+    wrapper.components.extend(
+        [input_latlng, DIV(_id="map", _style="width: 800px; height: 500px"), javascript]
+    )
+    return wrapper
+
 
 # -------------------------------------------------------------------------
 # Define your tables below (or better in another model file) for example
@@ -161,39 +221,78 @@ if configuration.get('scheduler.enabled'):
 # auth.enable_record_versioning(db)
 from widget import date_widget
 
-db.define_table('Provision',
-                Field('apellido'),
-                Field('nombre'),
-                Field('jerarquia'),
-                Field('numero_cargo', 'integer', label='Cargo'),
-                Field('numero_chapa', 'integer',),
-                Field('arma_provista'),
-                Field('marca'),
-                Field('calibre', ),
-                Field('numero_arma'),
-                Field('serie'),
-                Field('fecha_provision', 'string', widget=date_widget, default=request.now),
-                Field('recibo_numero'),
-                Field('cargadores'),
-                Field('fecha_provision_cartuchos', 'string', widget=date_widget),
-                Field('cantidad_cartuchos'),
-                Field('observaciones', 'text'),
-                Field('destino_actual'),
-                Field('uurr', label='UURR'),
-                Field('fecha_alta', 'string', widget=date_widget),
-                Field('numero_seguro_social'),
-                Field('fecha_nacimiento', 'string', widget=date_widget, label='Nacimiento'),
-                Field('numero_documento', 'integer', requires=IS_LENGTH(minsize=7), label='Documento'),
-                Field('cuil', 'integer'),
-                Field('domicilio'),
-                plural='Provisiones',
-                )
+db.define_table(
+    "jerarquia",
+    Field("nombre"),
+)
 
-db.define_table('Registro', # Registro de Transacciones
-                Field('user_id', db.auth_user),
-                Field('transaccion', 'string'),
-                )
+db.define_table(
+    "Provision",
+    Field("apellido"),
+    Field("nombre"),
+    Field("jerarquia", db.jerarquia, represent=lambda id, r: db.jerarquia[id].nombre),
+    Field(
+        "numero_cargo",
+        "integer",
+        label="Cargo",
+        requires=IS_NOT_EMPTY(),
+    ),
+    Field(
+        "numero_chapa",
+        "integer",
+    ),
+    Field("arma_provista"),
+    Field("marca"),
+    Field(
+        "calibre",
+    ),
+    Field("numero_arma"),
+    Field("serie"),
+    Field("fecha_provision", "string", widget=date_widget, default=request.now),
+    Field("recibo_numero", "integer"),
+    Field("cargadores", "integer"),
+    Field("fecha_provision_cartuchos", "string", widget=date_widget),
+    Field("cantidad_cartuchos", "integer"),
+    Field("observaciones", "text"),
+    Field("destino_actual"),
+    Field(
+        "uurr",
+        label="UURR",
+        requires=IS_IN_SET(["Capital", "Norte", "Sur", "Este", "Oeste"]),
+    ),
+    Field("fecha_alta", "string", widget=date_widget),
+    Field("numero_seguro_social", "integer"),
+    Field("fecha_nacimiento", "string", widget=date_widget, label="Nacimiento"),
+    Field(
+        "numero_documento",
+        "integer",
+        requires=IS_LENGTH(minsize=6, maxsize=8),
+        label="Documento",
+        required=True,
+    ),
+    Field("cuil", "integer"),
+    Field("domicilio", "string", widget=geocoder_widget),
+    plural="Provisiones",
+)
 
-db.Provision._after_update.append(lambda s,f: db.Registro.insert(user_id=auth.user_id, transaccion='Update: {}'.format(str(f))))
-db.Provision._after_delete.append(lambda s: db.Registro.insert(user_id=auth.user_id, transaccion='Delete: {}'.format(str(s))))
-db.Provision._after_insert.append(lambda f,id: db.Registro.insert(user_id=auth.user_id, transaccion='Insert: {}'.format(str(f))))
+db.define_table(
+    "Registro",  # Registro de Transacciones
+    Field("user_id", db.auth_user),
+    Field("transaccion", "string"),
+)
+
+db.Provision._after_update.append(
+    lambda s, f: db.Registro.insert(
+        user_id=auth.user_id, transaccion="Update: {}".format(str(f))
+    )
+)
+db.Provision._after_delete.append(
+    lambda s: db.Registro.insert(
+        user_id=auth.user_id, transaccion="Delete: {}".format(str(s))
+    )
+)
+db.Provision._after_insert.append(
+    lambda f, id: db.Registro.insert(
+        user_id=auth.user_id, transaccion="Insert: {}".format(str(f))
+    )
+)
